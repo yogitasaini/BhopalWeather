544dbaf2020a7b4a7be275435a6ec886fb8aab9f
yogita saini <yogitasaini2110@gmail.com>
2022-01-09 14:09:45 +0530

    weather

--- index.js
+++ index.js
@@ -1,48 +1,4 @@
-// require("dotenv").config();
-// const http = require("http");
-// const fs = require("fs");
-// var requests = require("requests");
 
-// const homeFile = fs.readFileSync("home.html", "utf-8");
-
-// const replaceVal = (tempVal, orgVal) => {
-//   let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
-//   temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
-//   temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
-//   temperature = temperature.replace("{%location%}", orgVal.name);
-//   temperature = temperature.replace("{%country%}", orgVal.sys.country);
-//   temperature = temperature.replace("{%tempStatus%}", orgVal.weather[0].main);
-
-//   return temperature;
-// };
-
-// const server = http.createServer((req, res) => {
-//   if (req.url == "/") {
-//     requests(
-//       `https://api.openweathermap.org/data/2.5/weather?q=Bhopal&appid=ea911b3d0e75bd6eec06693d2f0b8b66`
-//     )
-//       .on("data", (chunk) => {
-//         const objData = JSON.parse(chunk);
-//         const arrData = [objData];
-//         //console.log(arrData[0].main.temp);
-//         const realTimeDate = arrData
-//           .map((val) => replaceVal(homeFile, val))
-//           .join(" ");
-//         res.write(realTimeDate);
-//       })
-//       .on("end", (err) => {
-//         if (err) return console.log("connection closed due to errors", err);
-//         res.end;
-//       });
-//   } else {
-//     res.end("File not found");
-//   }
-// });
-
-// server.listen(8000, "127.0.0.1");
-
-
-//require('dotenv').config();
 const http = require("http");
 const fs = require("fs");
 var requests = require("requests");
